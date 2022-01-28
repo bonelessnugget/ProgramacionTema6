bef76e7459ce4604d6ad57bc918dfcf68a9e9cea
bonelessnugget <yszeedyt99@gmail.com>
2022-01-28 16:27:34 +0100

    Ejercicio 6

---
+++ Ejercicio6/src/Empleado.java
@@ -0,0 +1,22 @@
+/**
+ * Escribe un programa que utilice la clase Empleado, creando un vector de objetos de la clase
+Empleado y luego lo llene con datos correspondientes a 50 empleados (utilizar una tabla de
+nombres para rellenar con un random). Una vez rellenado, visualizar los datos de todos los
+empleados
+ * @author yszee
+ *
+ */
+public class Empleado {
+	
+	private String nombre;
+	private int numero = 0;
+	
+	public Empleado(String nombre, int numero) {
+		this.nombre = nombre;
+		this.numero = numero;
+	}
+	
+	public void verDatos() {
+		System.out.println("Nombre: " + nombre + " | " + "Numero: " + numero);
+	}
+}
---
+++ Ejercicio6/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Escribe un programa que utilice la clase Empleado, creando un vector de objetos de la clase
+Empleado y luego lo llene con datos correspondientes a 50 empleados (utilizar una tabla de
+nombres para rellenar con un random). Una vez rellenado, visualizar los datos de todos los
+empleados
+ * @author yszee
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Empleado[] empleado = new Empleado[50];
+		String[] nombres = new String[50];
+		
+		for (int i = 0; i<nombres.length; i++) {
+			nombres[i] = 
+		}
+
+	}
+
+}
