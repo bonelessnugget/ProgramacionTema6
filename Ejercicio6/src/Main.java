8f02a2fc90a282a3b96972fd15b7e3fb9b62af5d
bonelessnugget <yszeedyt99@gmail.com>
2022-01-29 12:51:11 +0100

    Vrei 5 lei pe exerhisio6?

--- Ejercicio6/src/Main.java
+++ Ejercicio6/src/Main.java
@@ -6,14 +6,42 @@ empleados
  * @author yszee
  *
  */
+import java.util.Random;
 public class Main {
-
+	public static String nombresRandom() {
+		String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		StringBuilder sb = new StringBuilder();
+		Random random = new Random();
+		int tamaño = (int) (Math.random()*(10-4)+4);
+		for (int i=0; i<tamaño; i++) {
+			int index = random.nextInt(caracteres.length());
+			char randomChar = caracteres.charAt(index);
+			sb.append(randomChar);
+			
+			
+			
+		}
+		String nombre = sb.toString();
+		return nombre;
+	}
 	public static void main(String[] args) {
 		Empleado[] empleado = new Empleado[50];
 		String[] nombres = new String[50];
+		int numero = -1;
 		
 		for (int i = 0; i<nombres.length; i++) {
-			nombres[i] = 
+			nombres[i] = nombresRandom();
+		}
+		for (int k = 0; k<empleado.length; k++) {
+			
+			numero++;
+			empleado[k] = new Empleado(nombres[k], numero);
+		}
+		
+		for (int j = 0; j<empleado.length; j++) {
+			empleado[j].verDatos();
+			
+			
 		}
 
 	}
